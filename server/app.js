45c71d4f0aea0c0d44b9d2d4807bcb0a584ea315
Daniel Tait <daniel.tait@skyscanner.net>
2015-06-17 10:30:39 +0100

    Added socket.io and setup basic connect / disconnect flow

--- server/app.js
+++ server/app.js
@@ -4,11 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var socketIo = require('socket.io');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var rooms = require('./routes/rooms');
 
 var app = express();
+
+var io = socketIo();
+app.io = io;
+
+io.on("connection", function(socket) {
+  console.log("A user connected");
+
+  socket.on('disconnect', function() {
+    console.log("A user disconnected");
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/rooms', rooms);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
